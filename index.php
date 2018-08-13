f0548a10bb45626e8ebd38339e5dd1c1e8f24e33
EllineView <elline_r@hotmail.com>
2018-08-13 12:19:41 +0300

    my files

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+//created by Elline_Romanchuk
+	echo 'Hello, Ell!' . '<br>';
+	print('Hello, Ella!') . '<br>';
+        var_dump('Hallo, Elline!!!'). '<br>';
+        var_dump('This is my 1st project'). '<br>';
+
+?>
