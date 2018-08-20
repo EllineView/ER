06bdd451910898fe42d4963a925ee465d065136d
EllineView <elline_r@hotmail.com>
2018-08-20 13:41:23 +0300

    Additional tasks of 2nd lesson

---
+++ index2_add.php
@@ -0,0 +1,11 @@
+<?php
+//created by Elline_Romanchuk
+echo "2nd lesson, additional tasks (from letter)" . '<br>';
+echo "The result of 1st task:" .'<br>';
+/*Вычислить периметр и площадь прямоугольника если вторая сторона в 4 раза больше первой (первую сторону придумать самому)*/
+$side1 = 25;
+$side2 = $side1*4;
+$per = ($side1 + $side2);
+echo "The perimetr of of a rectangle ($side1+$side2) is  : " .$per . '<br>';
+
+?>
