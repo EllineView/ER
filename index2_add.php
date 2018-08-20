bfa805875677a554a0886b40e4bca8e272bcda82
EllineView <elline_r@hotmail.com>
2018-08-20 13:46:21 +0300

    L2_add

--- index2_add.php
+++ index2_add.php
@@ -5,7 +5,8 @@ echo "The result of 1st task:" .'<br>';
 /*Вычислить периметр и площадь прямоугольника если вторая сторона в 4 раза больше первой (первую сторону придумать самому)*/
 $side1 = 25;
 $side2 = $side1*4;
-$per = ($side1 + $side2);
-echo "The perimetr of of a rectangle ($side1+$side2) is  : " .$per . '<br>';
-
+$per = ($side1 + $side2)*2;
+$sq  = $side1 * $side2;
+echo "The perimetr of of a rectangle ($side1+$side2)*2 is  : " .$per . '<br>';
+echo "The square of of a rectangle ($side1*$side2) is  : " .$sq . '<br>';
 ?>
