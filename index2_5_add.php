05d088cb1d7fda5ede931602cd5d9cc0c6f66c98
EllineView <elline_r@hotmail.com>
2018-08-20 14:51:59 +0300

    Les2 task5 add

---
+++ index2_5_add.php
@@ -0,0 +1,15 @@
+<?php
+//created by Elline_Romanchuk
+echo "2nd lesson, additional tasks (from letter)" . '<br>';
+echo "The result of 5th task:" .'<br>';
+/* Найти сумму квадратов  36% от первого числа и 87% от второго числа */
+$n1 = 0.75;
+$n2 = 15.26;
+$n3 = pow($n1*0.36,2) + pow($n2*0.87,2);
+
+echo "n1 is " . $n1 . '<br>';
+echo "n2 is " . $n2 . '<br>';
+echo "n3 is " . $n3 . '<br>';
+
+
+?>
